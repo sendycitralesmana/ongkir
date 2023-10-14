c61115d76da3c5ecd3384c16378af5364da303cc
Lesmana <sendycitralesmana@gmail.com>
2023-10-14 15:20:24 +0700

    Basic http Request

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,19 +4,33 @@ import 'package:http/http.dart' as http;
 import 'package:ongkir/app/data/models/user_model.dart';
 
 void main() async {
-  Uri url = Uri.parse("https://reqres.in/api/users/2");
-  final response = await http.get(url);
 
+  // initial
   // final data = (jsonDecode(response.body) as Map<String, dynamic>)['data']
   //     as Map<String, dynamic>;
 
   // print(data['first_name'] + "" + data['last_name']);
 
-  final user = UserModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  final data = user.data;
-  final support = user.support;
-  print("${data.firstName} ${data.lastName}");
+  // post
+  Uri url = Uri.parse("https://reqres.in/api/users");
+  final response =
+      await http.post(url, body: {"name": "morpheus", "job": "leader"});
+  print(response.body);
 
-  final myJson = userModelToJson(user);
-  print(myJson);
+  // get
+  // Uri url = Uri.parse("https://reqres.in/api/users/2");
+  // final response = await http.get(url);
+
+  // get
+  // final user =
+  //     UserModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  // final data = user.data;
+  // final support = user.support;
+  // print("${data.firstName} ${data.lastName}");
+
+
+
+  // 
+  // final myJson = userModelToJson(user);
+  // print(myJson);
 }
