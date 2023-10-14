a258e38bb661fcfc7caa4d9d386df80e65140ce1
Lesmana <sendycitralesmana@gmail.com>
2023-10-14 15:52:09 +0700

    Basic Dropdown Search

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -10,14 +11,29 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Ongkos Kirim Indonesia'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch<String>(
+            popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              showSearchBox: true,
+              // disabledItemFn: (String s) => s.startsWith('I'),
+            ),
+            items: ["Brazil", "Italia", "Tunisia", 'Canada'],
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "Kota Asal",
+                hintText: "Pilih Kota",
+              ),
+            ),
+            onChanged: (value) {},
+            // selectedItem: "Brazil",
+          ),
+        ],
       ),
     );
   }
